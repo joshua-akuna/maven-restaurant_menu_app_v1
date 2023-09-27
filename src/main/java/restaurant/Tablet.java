7cbf756ebb8f0981e3d70ae5319f30b7aa98fe37
Akuna Joshua <akunajoshua@gmail.com>
2023-09-27 13:22:41 +0100

    Tablet class doc added

--- src/main/java/restaurant/Tablet.java
+++ src/main/java/restaurant/Tablet.java
@@ -1,5 +1,9 @@
 package restaurant;
 
+/**
+ * <h2>Tablet!</h2>
+ * This class is responsible for all of the functionalities and properties for table.
+ */
 public class Tablet {
     private final int id;
 
@@ -7,6 +11,10 @@ public class Tablet {
         this.id = id;
     }
 
+    /**
+     * This method creates an Order instance consisting of the dishes order by the customer.
+     * It will utilize a simple text console to interact with the customer
+     */
     public void makeOrder(){
 
     }
