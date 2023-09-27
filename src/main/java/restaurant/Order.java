f105c7058181416290167a9d842542d476111369
Akuna Joshua <akunajoshua@gmail.com>
2023-09-27 13:24:02 +0100

    Order class doc added

--- src/main/java/restaurant/Order.java
+++ src/main/java/restaurant/Order.java
@@ -4,13 +4,24 @@ import restaurant.kitchen.Dish;
 import java.util.List;
 
 /**
- * Order - defines the customers order containing a list of dishes
+ * <h2>Order!</h2>
+ * Defines list of dishes ordered and the instance to the tablet where the order was made
+ *
+ * @author Akuna Joshua
+ * @version 1.0.0
+ * @since 2023-09-21
  */
 
 public class Order {
+    // and instance of the table from where the order was made
     private final Tablet tablet;
+    // a list of dishes ordered by the customer
     protected List<Dish> dishes;
 
+    /**
+     * the construct:or for the order class
+     * @param tablet: an instance of the tablet from where the customer placed the order
+     */
     public Order(Tablet tablet){
         this.tablet = tablet;
     }
