cea40273a8505e8a09189694a7871557edf7cefa
Akuna Joshua <akunajoshua@gmail.com>
2023-09-27 13:30:49 +0100

    Dish enum doc added

--- src/main/java/restaurant/kitchen/Dish.java
+++ src/main/java/restaurant/kitchen/Dish.java
@@ -1,21 +1,32 @@
 package restaurant.kitchen;
 
+/**
+ * <h2>Dish</h2>
+ * Defines and list available dishes
+ *
+ * @author Akuna Joshua
+ * @version 1.0.0
+ * @since 2023-09-21
+ */
 public enum Dish {
-    Fish,
-    Steak,
-    Soup,
-    Juice,
-    Water;
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
 
-    // returns a list of the available dishes
+    /**
+     * Returns a String listing all the available dishes
+     * @return a string listing all the available dishes.
+     */
     public static String allDishesToString(){
         return String.format(
                 "%s, %s, %s, %s, %s.",
-                Fish,
-                Steak,
-                Soup,
-                Juice,
-                Water
+                FISH,
+                STEAK,
+                SOUP,
+                JUICE,
+                WATER
         );
     }
 }
